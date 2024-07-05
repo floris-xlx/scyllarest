1e84bf5e7643a7a139aaddcef55fded3d6fe17f4
floris-xlx <floris@xylex.ai>
2024-07-05 23:12:57 +0200

    chore: Add anyhow crate dependency and implement get_table_columns method

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,56 @@
+use crate::ScyllaClient;
+use scylla::IntoTypedRows;
+use scylla::QueryResult;
+use scylla::{Session, SessionBuilder};
+use serde_json::{json, Value};
+use std::error::Error;
 
+impl ScyllaClient {
+    pub async fn new(known_nodes: Vec<&str>) -> Result<Self, Box<dyn Error + Send + Sync>> {
+        let mut builder = SessionBuilder::new();
+
+        for node in known_nodes {
+            builder = builder.known_node(node);
+        }
+
+        let session: Session = builder.build().await?;
+
+        Ok(ScyllaClient { session })
+    }
+
+    // pub fn query(&self, keyspace: &str, table: &str) -> QueryBuilder<'_> {
+    //     QueryBuilder::new(Operations::Select, keyspace, table, self)
+    // }
+
+    pub async fn get_table_columns(
+        &self,
+        keyspace: &str,
+        table: &str,
+    ) -> Result<Value, Box<dyn Error + Send + Sync>> {
+        let query: String = format!(
+            "SELECT column_name, kind, type FROM system_schema.columns WHERE keyspace_name = '{}' AND table_name = '{}'",
+            keyspace, table
+        );
+
+        let result: QueryResult = self.session.query(query, ()).await?;
+        let rows: Vec<scylla::frame::response::result::Row> = result.rows.ok_or("No rows found")?;
+
+        let mut columns: Vec<Value> = vec![];
+        for row in rows.into_typed::<(String, String, String)>() {
+            let (column_name, kind, data_type) = row?;
+            columns.push(json!({
+                "column_name": column_name,
+                "kind": kind,
+                "data_type": data_type,
+            }));
+        }
+
+        let json_result: Value = json!({
+            "keyspace": keyspace,
+            "table": table,
+            "columns": columns,
+        });
+
+        Ok(json_result)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder, Result };
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
 use dotenv::dotenv;
 
+use scyllarest::ScyllaClient;
+use serde_json::Value;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    let port: u16 = std::env::var("PORT").unwrap_or("8080".to_string()).parse().unwrap();
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .unwrap();
     println!("Starting server on port {}", port);
 
-    HttpServer::new(|| { 
-        App::new()
-        .service(hello) 
-        .service(rest_root)
-    })
-
+    HttpServer::new(|| App::new().service(hello).service(rest_root))
         .bind(("127.0.0.1", port))?
-        .run().await
+        .run()
+        .await
 }
 
 #[get("/")]
@@ -24,19 +25,66 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-
 #[get("/rest/v1/{keyspace}/{table}")]
 async fn rest_root(path: web::Path<(String, String)>) -> Result<HttpResponse> {
-    let (
-        keyspace,
-        table
-    ) = path.into_inner();
-    
+    let (keyspace, table) = path.into_inner();
+
+    // auth to scylladb
+    let client_auth_state = match auth_client_scylladb().await {
+        Ok(_) => {
+            println!("Auth success");
+            "success"
+        }
+        Err(e) => {
+            println!("Auth failed: {}", e);
+            "failed"
+        }
+    };
+
+    // get table columns and return as json
+    let columns: Value = match getting_table_columns(
+        &keyspace,
+        &table,
+    ).await {
+        Ok(columns) => {
+            println!("\x1b[32mGetting table columns success\x1b[0m");
+            columns
+        }
+        Err(e) => {
+            println!("\x1b[31mGetting table columns failed: {}\x1b[0m", e);
+            serde_json::Value::Null
+        }
+    };
+
+    // get the columns key from the json
+    let column_values = columns.get("columns").unwrap();
+
+
     let response: serde_json::Value = serde_json::json!({
         "route": "/rest/v1",
         "keyspace": keyspace,
-        "table": table
+        "table": table,
+        "client_auth_state": client_auth_state,
+        "table_columns": column_values,
+        "ssl": "true"
     });
-
     Ok(HttpResponse::Ok().json(response))
+}
+
+pub async fn auth_client_scylladb() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let _client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+    Ok(())
+}
+
+async fn getting_table_columns(
+    keyspace: &str,
+    table: &str,
+) -> Result<Value, Box<dyn std::error::Error + Send + Sync>> {
+    let client: ScyllaClient = ScyllaClient::new(vec!["127.0.0.1"]).await?;
+
+    let columns: serde_json::Value = client
+        .get_table_columns(keyspace, table)
+        .await?;
+
+    Ok(columns)
 }
